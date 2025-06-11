d7c884fe95a1599e6f4091745b4c5d0409824dc3
David Li <taweili@gmail.com>
2025-06-11 22:37:54 +0800

    can calibrate arms one by one.

--- my-notes/sa100-calibrate.sh
+++ my-notes/sa100-calibrate.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
 
+# Parse arguments
+while [[ $# -gt 0 ]]; do
+    case "$1" in
+        --arms)
+            arms="$2"
+            shift 2
+            ;;
+        *)
+            echo "Unknown argument: $1"
+            exit 1
+            ;;
+    esac
+done
+
+# Default to both arms if not specified
+arms=${arms:-"right left"}
+
 python lerobot/scripts/control_robot.py \
     --robot.type=sa100 \
-    --control.type=calibrate
+    --control.type=calibrate \
+    --control.arms $arms  # Use specified arms or default to both
